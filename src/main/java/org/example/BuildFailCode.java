ec43553a4eff38831b5a111f9d1b44d65fe92b9b
Kshitij Kumar <kshitij.kumar@modmed.com>
2023-03-14 04:21:29 -0400

    trying to fail build

--- src/main/java/org/example/BuildFailCode.java
+++ src/main/java/org/example/BuildFailCode.java
@@ -4,6 +4,6 @@ public class BuildFailCode {
 
     public static void main(String[] args) {
         
-        System.out.println("Hello Check");
+        System.out.println("Hello World")
     }
 }
