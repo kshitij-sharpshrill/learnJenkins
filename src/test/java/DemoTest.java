d49fc0b8748640864171b036b091a44298dea806
Kshitij Kumar <kshitij.kumar@modmed.com>
2023-03-20 02:28:44 -0400

    removed semicolon

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -6,6 +6,6 @@ public class DemoTest {
     @Test(description = "Creating TestNG Demo code to run on Jenkins")
     public void testCodeRun(){
         System.out.println("Hello! I have started testing");
-        Assert.assertTrue(true)
+        Assert.assertTrue(true);
     }
 }
