c3ff5f723e90ac6596f9c4e0c5f781fd02513f89
Kshitij Kumar <kshitij.kumar@modmed.com>
2024-05-30 02:24:26 -0400

    wait code update

--- src/test/java/DemoSlackIntegrationTest.java
+++ src/test/java/DemoSlackIntegrationTest.java
@@ -11,12 +11,15 @@ public class DemoSlackIntegrationTest {
     public void demoTest() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://google.com");
-        try {
-            driver.wait(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        WebElement googleSearch = driver.findElement(By.xpath("(//input[@value='Google Search'])[2]"));
+        synchronized (googleSearch) {
+            try {
+                googleSearch.wait(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        WebElement googleSearch = driver.findElement(By.xpath("(//input[@value='Google Search'])[1]"));
         Assert.assertTrue(googleSearch.isDisplayed());
+        driver.quit();
     }
 }
